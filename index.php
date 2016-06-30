202c2276eaa99e71da92107a08e93d740aaf9b6c
pazix Pazz <fidan.hysaj@gmail.com>
2016-06-30 12:15:44 +0200

    unlink file_exists is_dir
    supprime vérifie un fichier et un répertoire

--- index.php
+++ index.php
@@ -24,4 +24,16 @@ $content = file_get_contents("texte.log");
 $content .= date("Y-m-d H:i:s").": FCP le script a été ouvert\r\n";
 file_put_contents("texte.log", $content);
 
+// pour supprimer un fichier
+//unlink("text.log");
+
+// vérifier si un fichier existe ou non
+if(file_exists("texte.log")){
+	echo "Le Fichier de log existe"
+}
+
+// vérifier si un repertoire var existe
+if(!is_dir('var')){
+	mkdir('var');
+}
 ?>
