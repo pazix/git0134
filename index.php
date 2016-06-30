dd68cea893033e00a94644d15b07ad5b76eae0cd
pazix Pazz <fidan.hysaj@gmail.com>
2016-06-30 11:35:33 +0200

    fwritetxtlog

--- index.php
+++ index.php
@@ -15,5 +15,8 @@
 $file = fopen("texte.txt", "r");
 var_dump(fread($file,filesize("texte.txt")));
 fclose($file);
+$file= fopen("texte.log", "w+");
+fwrite($file, "Mon contenu");
+fclose($file);
 
 ?>
