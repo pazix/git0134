4bbc9e72bd6f3544f41579d228f9625be582480a
pazix Pazz <fidan.hysaj@gmail.com>
2016-06-30 11:40:53 +0200

    fopen("a")
    Ajout en plus dans le fichier log

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
 $file = fopen("texte.txt", "r");
 var_dump(fread($file,filesize("texte.txt")));
 fclose($file);
-$file= fopen("texte.log", "w+");
-fwrite($file, "Mon contenu");
+$file= fopen("texte.log", "a");
+fwrite($file, date("Y-m-d H:i:s").": le script a été ouvert\r\n");
 fclose($file);
 
 ?>
