2df366c6909a0788354e54ba368df75358be5434
pazix Pazz <fidan.hysaj@gmail.com>
2016-06-30 11:59:53 +0200

    file_get_contents
    Fonctions équivalents a fopen

--- index.php
+++ index.php
@@ -11,12 +11,15 @@
 </body>
 </html>
 <?php 
-
+// pour lire  un fichier 
 $file = fopen("texte.txt", "r");
 var_dump(fread($file,filesize("texte.txt")));
 fclose($file);
+// pour ecrire dans un fichier 
 $file= fopen("texte.log", "a");
 fwrite($file, date("Y-m-d H:i:s").": le script a été ouvert\r\n");
 fclose($file);
+// fonctions équivalentes
+echo file_get_contents("texte.log");
 
 ?>
